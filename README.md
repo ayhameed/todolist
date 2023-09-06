a19fe50535668520cc80d32338bc8e889bf65f0a
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2023-09-06 14:39:36 +0100

    added I/O ops to MongoDB

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 To get started run : node app.js
 EJS is the template engine used 
+
+
+## Notes
+- env file is included so you can edit the env variables
+
+## whats new?
+ - Read and Write to mongodb using Mongoose
--- app.js
+++ app.js
@@ -1,37 +1,83 @@
-const express = require("express")
-// const bodyParser = require("body-parser")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-const app = express()
+app.set("view engine", "ejs");
 
-app.set('view engine', 'ejs')
-
-let items = []
 app.use(express.json());
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT;
+const MongoDB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
 
-app.get("/", (req, res)=>{
+mongoose
+    .connect(MongoDB_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connection to MongoDB successful");
+    })
+    .catch((error) => {
+        console.error("MongoDB Connection error:", error);
+    });
 
-    let today = new Date()
-    let options = { 
-        weekday: 'long',
-        day: 'numeric', 
-        month: 'long' 
+const TodoItemSchema = new mongoose.Schema({
+    item: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+});
+
+const TodoItem = mongoose.model("TodoItem", TodoItemSchema);
+
+const dateFunction = () => {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    };
+    return today.toLocaleDateString("en-US", options);
+};
+
+app.get("/", async (req, res) => {
+    const day = dateFunction();
+    try {
+        const items = await TodoItem.find();
+        res.render("list", { kindOfDay: day, items: items });
+    } catch (error) {
+        console.error("Error fetching items:", error);
+        res.status(500).send("Internal Server Error");
     }
-    let toDay  = new Date();
-    let day = toDay.toLocaleDateString("en-US", options)
-    res.render("list",{kindOfDay: day, item : items})
-})
+});
 
-app.post("/", (req, res)=>{
-    const newTodoItem = req.body.newItem
-    items.push(newTodoItem)
-    res.redirect("/")
-})
+app.post("/", async (req, res) => {
+    const newTodoItem = req.body.newItem;
+    const date = dateFunction();
 
-app.listen(PORT, ()=>{
-    console.log("sever is listening on Port: " +PORT)
-})
+    // Write to Collection
+    const todoitem = new TodoItem({
+        item: newTodoItem,
+        date: date,
+    });
+
+    try {
+        await todoitem.save();
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error saving item:", error);
+        res.redirect("/");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log("Server is listening on Port: " + PORT);
+});
